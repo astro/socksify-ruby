f37c79c6171f761fbd3ad8ea4605f74885c01db1
Andrey Kouznetsov <smixok@gmail.com>
2010-10-18 06:33:33 -0700

    Socks 4a support

--- lib/socksify.rb
+++ lib/socksify.rb
@@ -90,6 +90,13 @@ class SOCKSError < RuntimeError
 end
 
 class TCPSocket
+  def self.socks_version
+    @@socks_version ||= 5
+    @@socks_version == 4 ? "\004" : "\005"
+  end
+  def self.socks_version=(version)
+    @@socks_version = version
+  end
   def self.socks_server
     @@socks_server ||= nil
   end
@@ -121,7 +128,7 @@ class TCPSocket
       Socksify::debug_notice "Connecting to SOCKS server #{socks_server}:#{socks_port}"
       initialize_tcp socks_server, socks_port
 
-      socks_authenticate
+      socks_authenticate unless @@socks_version == 4
 
       if host
         socks_connect(host, port)
@@ -150,20 +157,33 @@ class TCPSocket
   # Connect
   def socks_connect(host, port)
     Socksify::debug_debug "Sending destination address"
-    write "\005\001\000"
+    write TCPSocket.socks_version
+    write "\001"
+    write "\000" if @@socks_version == 5
+    write [port].pack('n') if @@socks_version == 4
+
     if host =~ /^(\d+)\.(\d+)\.(\d+)\.(\d+)$/  # to IPv4 address
-      write "\001" + [$1.to_i,
-                      $2.to_i,
-                      $3.to_i,
-                      $4.to_i
-                     ].pack('CCCC')
+      write "\001" if @@socks_version == 5
+      _ip = [$1.to_i,
+             $2.to_i,
+             $3.to_i,
+             $4.to_i
+            ].pack('CCCC')
+      write _ip
     elsif host =~ /^[:0-9a-f]+$/  # to IPv6 address
       raise "TCP/IPv6 over SOCKS is not yet supported (inet_pton missing in Ruby & not supported by Tor"
       write "\004"
     else                          # to hostname
-      write "\003" + [host.size].pack('C') + host
+      if @@socks_version == 5
+        write "\003" + [host.size].pack('C') + host
+      else
+        write "\000\000\000\001"
+        write "\001\000"
+        write host
+        write "\000"
+      end
     end
-    write [port].pack('n')
+    write [port].pack('n') if @@socks_version == 5
 
     socks_receive_reply
     Socksify::debug_notice "Connected to #{host}:#{port} over SOCKS"
@@ -172,44 +192,52 @@ class TCPSocket
   # returns [bind_addr: String, bind_port: Fixnum]
   def socks_receive_reply
     Socksify::debug_debug "Waiting for SOCKS reply"
-    connect_reply = recv(4)
-    if connect_reply[0..0] != "\005"
-      raise SOCKSError.new("SOCKS version #{connect_reply[0..0]} is not 5")
-    end
-    if connect_reply[1..1] != "\000"
-      raise SOCKSError.for_response_code(connect_reply.bytes.to_a[1])
-    end
-    Socksify::debug_debug "Waiting for bind_addr"
-    bind_addr_len = case connect_reply[3..3]
-                    when "\001"
-                      4
-                    when "\003"
-                      recv(1).bytes.first
-                    when "\004"
-                      16
-                    else
-                      raise SOCKSError.for_response_code(connect_reply.bytes.to_a[3])
-                    end
-    bind_addr_s = recv(bind_addr_len)
-    bind_addr = case connect_reply[3..3]
-                when "\001"
-                  bind_addr_s.bytes.to_a.join('.')
-                when "\003"
-                  bind_addr_s
-                when "\004"  # Untested!
-                  i = 0
-                  ip6 = ""
-                  bind_addr_s.each_byte do |b|
-                    if i > 0 and i % 2 == 0
-                      ip6 += ":"
-                    end
-                    i += 1
+    if @@socks_version == 5
+      connect_reply = recv(4)
+      if connect_reply[0..0] != "\005"
+        raise SOCKSError.new("SOCKS version #{connect_reply[0..0]} is not 4 or 5")
+      end
+      if connect_reply[1..1] != "\000"
+        raise SOCKSError.for_response_code(connect_reply.bytes.to_a[1])
+      end
+      Socksify::debug_debug "Waiting for bind_addr"
+      bind_addr_len = case connect_reply[3..3]
+                      when "\001"
+                        4
+                      when "\003"
+                        recv(1).bytes.first
+                      when "\004"
+                        16
+                      else
+                        raise SOCKSError.for_response_code(connect_reply.bytes.to_a[3])
+                      end
+      bind_addr_s = recv(bind_addr_len)
+      bind_addr = case connect_reply[3..3]
+                  when "\001"
+                    bind_addr_s.bytes.to_a.join('.')
+                  when "\003"
+                    bind_addr_s
+                  when "\004"  # Untested!
+                    i = 0
+                    ip6 = ""
+                    bind_addr_s.each_byte do |b|
+                      if i > 0 and i % 2 == 0
+                        ip6 += ":"
+                      end
+                      i += 1
 
-                    ip6 += b.to_s(16).rjust(2, '0')
+                      ip6 += b.to_s(16).rjust(2, '0')
+                    end
                   end
-                end
-    bind_port = recv(bind_addr_len + 2)
-    [bind_addr, bind_port.unpack('n')]
+      bind_port = recv(bind_addr_len + 2)
+      [bind_addr, bind_port.unpack('n')]
+    else
+      connect_reply = recv(8)
+      unless connect_reply[0] == "\000" and connect_reply[1] == "\x5A"
+        Socksify::debug_debug connect_reply.unpack 'H'
+        raise SOCKSError.new("Failed while connecting througth socks")
+      end
+    end
   end
 end
 
