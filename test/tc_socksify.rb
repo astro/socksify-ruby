d71719e390b4fe660bef3194c133e889dc447615
Musy Bite <musybite@gmail.com>
2014-08-25 15:12:29 +0400

    Fix specs: check.torproject.org does not respond to IP address any more.
    Use https://internet.yandex.com/ for checking connection to IP instead.

--- test/tc_socksify.rb
+++ test/tc_socksify.rb
@@ -28,35 +28,50 @@ class SocksifyTest < Test::Unit::TestCase
   end
 
   def test_check_tor
-    [['Hostname', :check_tor],
-     ['IPv4', :check_tor_ip]].each do |f_name, f|
-      disable_socks
+    disable_socks
 
-      tor_direct, ip_direct = send(f)
-      assert_equal(false, tor_direct)
+    is_tor_direct, ip_direct = check_tor
+    assert_equal(false, is_tor_direct)
 
-      enable_socks
+    enable_socks
 
-      tor_socks, ip_socks = send(f)
-      assert_equal(true, tor_socks)
+    is_tor_socks, ip_socks = check_tor
+    assert_equal(true, is_tor_socks)
 
-      assert(ip_direct != ip_socks)
-    end
+    assert(ip_direct != ip_socks)
   end
 
   def test_check_tor_via_net_http
     disable_socks
 
-    [['Hostname', :check_tor],
-     ['IPv4', :check_tor_ip]].each do |f_name, f|
-      tor_direct, ip_direct = send(f)
-      assert_equal(false, tor_direct)
+    tor_direct, ip_direct = check_tor
+    assert_equal(false, tor_direct)
 
-      tor_socks, ip_socks = send(f, http_tor_proxy)
-      assert_equal(true, tor_socks)
+    tor_socks, ip_socks = check_tor(http_tor_proxy)
+    assert_equal(true, tor_socks)
 
-      assert(ip_direct != ip_socks)
-    end
+    assert(ip_direct != ip_socks)
+  end
+
+  def test_connect_to_ip
+    disable_socks
+
+    ip_direct = internet_yandex_com_ip
+
+    enable_socks
+
+    ip_socks = internet_yandex_com_ip
+
+    assert(ip_direct != ip_socks)
+  end
+
+  def test_check_tor_via_net_http
+    disable_socks
+
+    ip_direct = internet_yandex_com_ip
+    ip_socks = internet_yandex_com_ip(http_tor_proxy)
+
+    assert(ip_direct != ip_socks)
   end
 
   def test_ignores
@@ -74,14 +89,14 @@ class SocksifyTest < Test::Unit::TestCase
     assert(ip_direct == ip_socks_ignored)
   end
 
-  def get_http(http_klass, url)
+  def get_http(http_klass, url, host_header)
     uri = URI(url)
     body = nil
     http_klass.start(uri.host, uri.port,
                      :use_ssl => uri.scheme == 'https',
                      :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
       req = Net::HTTP::Get.new uri.request_uri
-      req['Host'] = "check.torproject.org"
+      req['Host'] = host_header
       req['User-Agent'] = "ruby-socksify test"
       body = http.request(req).body
     end
@@ -89,11 +104,11 @@ class SocksifyTest < Test::Unit::TestCase
   end
 
   def check_tor(http_klass = Net::HTTP)
-    parse_check_response get_http(http_klass, 'https://check.torproject.org/')
+    parse_check_response get_http(http_klass, 'https://check.torproject.org/', 'check.torproject.org')
   end
 
-  def check_tor_ip(http_klass = Net::HTTP)
-    parse_check_response get_http(http_klass, 'https://38.229.72.22/')  # "check.torproject.org"
+  def internet_yandex_com_ip(http_klass = Net::HTTP)
+    parse_internet_yandex_com_response get_http(http_klass, 'https://213.180.204.248/', 'internet.yandex.com') # "internet.yandex.com"
   end
 
   def parse_check_response(body)
@@ -111,6 +126,15 @@ class SocksifyTest < Test::Unit::TestCase
       raise 'Bogus response, no IP'
     end
     [is_tor, ip]
+  end
+
+  def parse_internet_yandex_com_response(body)
+    if body =~ /My IPv4: (\d+\.\d+\.\d+\.\d+)/
+      ip = $1
+    else
+      raise 'Bogus response, no IP'
+    end
+    ip
   end
 
   def test_resolve
